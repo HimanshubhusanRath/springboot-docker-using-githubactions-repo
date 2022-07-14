88ad1dc7e41742bc83879b163fa16780cd084cb2
Himanshu Bhusan Rath <himanshubhusanrath@admins-MacBook-Pro-4.local>
2022-07-14 11:20:28 +0530

    initial commit

---
+++ src/main/java/com/hr/springboot/github/actions/SpringbootDockerUsingGithubActionsApplication.java
@@ -0,0 +1,23 @@
+package com.hr.springboot.github.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerUsingGithubActionsApplication {
+
+	@GetMapping
+	public String message()
+	{
+		return "Hello User";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerUsingGithubActionsApplication.class, args);
+	}
+
+}
